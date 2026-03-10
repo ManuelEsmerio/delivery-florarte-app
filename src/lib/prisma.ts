cbbe88428ecb398a8192141187b4c95492d914b6
Manuel Esmerio <manuel.esmerio1995@gmail.com>
2026-03-10 22:36:14 +0000

    Los datos de los modelos no son como yo los tengo en la base de datos

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
-
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// Cliente Singleton para evitar múltiples conexiones en desarrollo (Next.js Hot Reload)
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
