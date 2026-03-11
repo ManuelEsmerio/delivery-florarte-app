7bb48a6455a5942783574aafaaeb807bc5018551
Manuel Esmerio <manuel.esmerio1995@gmail.com>
2026-03-11 22:50:33 +0000

    usa estos colores para toda la aplicacion.
    @tailwind base;
    @tailwind co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata, Viewport} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
@@ -18,7 +17,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#ec5b13',
+  themeColor: '#f472b6',
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
